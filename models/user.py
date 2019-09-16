16fc4e1761f77ea79addb0ffd7aa1525e9fea39b
Rmolimock <495@holbertonschool.com>
2019-09-16 05:08:21 +0000

    redoing project from scratch to solidify understanding

--- models/user.py
+++ models/user.py
@@ -1,25 +1,15 @@
 #!/usr/bin/python3
-"""This module contains the User class for
-which inherits from BaseModel
-"""
+''' This module contains one class, User '''
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """User class for Holberton BnB project
-    Args:
-        BaseModel: Inherits from BaseModel class
-    """
-
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    ''' This class defines a user of the application '''
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
 
     def __init__(self, *args, **kwargs):
-        """Initializes the User obj
-        Args:
-            args: args
-            kwargs: Dictionary of object attributes
-        """
+        ''' Initialize a user with BaseModel's atts/methods '''
         super().__init__(self, *args, **kwargs)
